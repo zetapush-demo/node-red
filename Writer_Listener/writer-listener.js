20e37fd87aac50ef5267e23c5e967f5cf8bf2a53
hippolyte lacassagne <hippolytelacassagne@MacBook-Air-de-hippolyte.local>
2017-12-07 18:14:10 +0100

    writer Listener finished

--- Writer_Listener/writer-listener.js
+++ Writer_Listener/writer-listener.js
@@ -2,16 +2,36 @@ module.exports = function(RED) {
   function WriterListenerNode(config) {
     RED.nodes.createNode(this,config);
     var node = this;
-    node.on('input', function(msg) {
+    var flowContext = node.context().flow;
 
-      var flowContext = node.context().flow;
+    node.on('input', function(msg) {
       var service =flowContext.get("service");
 
       service.call({
 			 name: config.name,
 			 parameters: {temperature : msg.payload}
 	    });
+
     });
+
+    var ZetaPush = require('zetapush-js');
+    var client =flowContext.get("client");
+
+    var listener = {};  //Creer un objet vide
+
+    listener[config.out] = function(response) {
+      console.log("response : " +JSON.stringify(response));
+
+      var msg = {};
+      msg.payload = response.data.name;
+      node.send(msg);
+    };
+    
+    client.createService({
+      Type: ZetaPush.services.Macro,
+      listener: listener
+    });
+
   }
   RED.nodes.registerType("writer-listener",WriterListenerNode);
 }
